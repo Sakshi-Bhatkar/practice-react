155925824bd72bce8fc49c407d37b584d2a65e89
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2023-04-19 22:19:56 +0530

    Named Export and Default Export

---
+++ module1.mjs
@@ -0,0 +1,12 @@
+const a ="Sakshi";
+const b="Param";
+const c="Raj";
+const d="Sanjana";
+
+
+export default b;
+export {a};
+export {c};
+export {d};
+
+//An MJS file is a source code file containing an ES Module (ECMAScript Module) for use with a Node.js application
---
+++ module2.mjs
@@ -0,0 +1,5 @@
+import dd, {a, c, d} from './module1.mjs'
+console.log(a);
+console.log(dd);
+console.log(c);
+console.log(d);
