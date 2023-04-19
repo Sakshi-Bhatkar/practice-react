bce95cbc2b7b6e48fa0c8b34a22fd677f4d8088f
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2023-04-19 23:19:07 +0530

    4-Props
    Props are arguments passed into React components.

--- App.js
+++ App.js
@@ -1,41 +1,9 @@
-
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="/">My Page</a>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                 <span className="navbar-toggler-icon"></span>
-              </button>
-            <diCv className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                   <a className="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/">About</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">Contact</a>
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="/">Mail-Id</a></li>
-                            <li><a className="dropdown-item" href="/">Phone No</a></li>
-                            <li><hr className="dropdown-divider" /></li>
-                            <li><a className="dropdown-item" href="/">Custom message</a></li>
-                        </ul>
-                </li>
-              </ul>
-              <form className="d-flex" role="search">
-                  <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                  <button className="btn btn-outline-success" type="submit">Search</button>
-              </form>
-           </diCv>
-        </div>
-      </nav>
-    </>
+    <Navbar title= "My Page" about="About Us"/>
   );
 }
 
