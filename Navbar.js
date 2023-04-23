e177a6a879c76027bf58eceb4c20129ae1be76c3
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2023-04-23 22:11:54 +0530

    Word Count, Time, LowerCase, navbar-dark

--- Navbar.js
+++ Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 
 export default function Navbar(props) {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
           <a className="navbar-brand" href="/">{props.title}</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,7 +30,7 @@ export default function Navbar(props) {
             </ul>
             <form className="d-flex" role="search">
                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
+                <button className="btn btn-outline-primary" type="submit">Search</button>
             </form>
          </div>
       </div>
@@ -38,11 +38,11 @@ export default function Navbar(props) {
   </>
   );
 }
-Navbar.propTypes={
+/* Navbar.propTypes={
   title : PropTypes.string.isRequired,
   about : PropTypes.string.isRequired
 }
-/* Navbar.defaultProps={
+ Navbar.defaultProps={
   title : 'Title goes here',
   about : 'About Us here'
 } */
--- TextForm.js
+++ TextForm.js
@@ -1,26 +1,45 @@
 import React, {useState} from 'react';
 
-
+//{/*Uppercase*/}
 export default function TextForm(props) {
   const handleUpClick= ()=> {
-    console.log("Uppercase was clicked" + text);
+    //console.log("Uppercase was clicked" + text);
     let newText= text.toUpperCase();
     setText(newText);
   }
-
+  //{/*Lowercase*/}
+  const handlelowClick= ()=> {
+    //console.log("Lowercase was clicked" + text);
+    let newText= text.toLowerCase();
+    setText(newText);
+  }
+  //onChange handler
   const handleOnChange= (event)=> {
     console.log("On change");
     setText(event.target.value);
   }
-
-  const [text, setText] = useState(' ');
+  //Mail box
+  const [text, setText] = useState('');
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
        <div className="mb-3">
           <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary" onClick={handlelowClick}>Convert to LowerCase</button>
+
     </div>
+    {/*word count*/}
+    <div className="container my-3">
+        <h2>Text Count</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length } Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+
   );
 }
